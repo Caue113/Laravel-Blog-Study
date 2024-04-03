cff10dddd09d4c93b54114041cd2ebd7c7cfd546
Luis Felipe <luisftsat@gmail.com>
2024-04-03 20:36:39 -0300

    feat: route get view with passing data in route function

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,26 @@ Route::get('/', function () {
 
 /* Users zone. No regex validation needed */
 Route::get("/users", function(){
-    return response("<h1>Users</h1>");
+    return view("userListing", [
+        "heading" => "Some nice text",
+        "users" => [
+            [
+                "id" => 1,
+                "name" => "John",
+                "bio" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptates quod dolore quam sint ex nobis nisi laboriosam quae facere cumque ratione, placeat labore? Repellendus, porro. Obcaecati dolore nulla id."
+            ],
+            [
+                "id" => 2,
+                "name" => "Marie",
+                "bio" => "Repellendus, porro. Obcaecati dolore nulla id."
+            ],
+            [
+                "id" => 3,
+                "name" => "Austin",
+                "bio" => "Hello id"
+            ]
+        ]
+    ]);
 });
 
 Route::get("/user/{userHandle}", function($userHandle){
@@ -35,8 +54,8 @@ Route::get("/posts", function(){
 Route::get("/posts/{id}", function($id){
     //dd();
     //ddd();
-    if(is_null($id)){
-        return response("No user found", 400,)->header("Content-Type", "text/plain");
+    if($id > 9 || $id < 0){
+        return response("No user found", 404,)->header("Content-Type", "text/plain");
     }
     return response("This Post: " . $id);
 })->where("id", "[0-9]+");
@@ -44,6 +63,7 @@ Route::get("/posts/{id}", function($id){
 
 /* Other Routes */
 
+//Function can take Request as parameters, which will show a data structure of a Request. All of them are accessible
 Route::get("/search", function(Request $req){
     dd($req);
 });
