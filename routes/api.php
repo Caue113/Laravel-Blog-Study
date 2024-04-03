5e0bfe8684cb3e693fbaff95b656650e063e7558
Luis Felipe <luisftsat@gmail.com>
2024-04-03 20:38:30 -0300

    feat minor: self test to create files inside repo when someone requests a url.
    Side effects could be used, such as database tracking usage by user, quantity in total, etc

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/posts", function(){
+
+    file_put_contents(dirname(__DIR__) . "/_myFiles/myText.txt", "Times accessed: ");
+
     return response()->json([
         "posts" =>[
+            "this dir3" => dirname(__DIR__),
             [
                 "title" => "A sunny day in Miami",
                 "date" => "Today"
