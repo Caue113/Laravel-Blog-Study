edce9d3222292a143c7474e73d68dabacae89ebd
Luis Felipe <luisftsat@gmail.com>
2024-04-05 13:51:53 -0300

    feat frontend: created basic footer in main layout
    Its intended to make this footer a component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,15 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bloggest - Your Blog</title>
+
+
+    <link rel="stylesheet" href="/css/footer.css">
 </head>
 <body>
     {{-- Header  --}}
-    <h1><i>'logo'</i>Bloggest</h1>
+    <header>
+        <h1><i>'logo'</i>Bloggest</h1>
+        <div></div>
+        {{-- Navigation across site. Substitute with partial or component --}}
+        <nav></nav>
+    </header>
+    
     
     {{-- Slot to insert other views inside  --}}
     @yield('content')
 
     {{-- Footer --}}
-    {{-- Stuff stuff stuff --}}
+    <footer class="footer">
+        <div class="footer__nav">
+            <div class="guide">
+                <h3 class="guide__title">Assistance</h3>
+                <ul>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">QnA</a></li>
+                </ul>
+            </div>
+            <div class="guide">
+                <h3 class="guide__title">Account</h3>
+                <ul>
+                    <li><a href="#">Create content</a></li>
+                    <li><a href="#">Sponsors</a></li>
+                    <li><a href="#">Avaliate Us</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer__credits">
+            <p class="footer__creator">Made by Luis Felipe</p>
+        </div>
+    </footer>
 </body>
 </html>
