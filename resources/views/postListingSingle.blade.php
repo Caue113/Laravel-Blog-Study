8f4ae0917c293b3d20606b8ad8569b6dba92b02f
Luis Felipe <luisftsat@gmail.com>
2024-04-05 11:17:40 -0300

    feat: data validation on single post view on nonexistant id

--- resources/views/postListingSingle.blade.php
+++ resources/views/postListingSingle.blade.php
@@ -1,14 +1,23 @@
 <h1>Post</h1>
 
-{{-- Alternatively, you can use @unless @endunless, which works simlarly to IF-ELSE --}}
+
+
+
+@if(is_null($post) == 1)
+
+<p>Post not found :(</p>
+
+@else
+
 <article style="border: black 1px dashed">
     <p>post_id: {{$post['id']}}</p>
     <p>post_title: {{$post['title']}}</p>
     <p>post_owner: {{$post['owner']}}</p>
+
+    <p>Posted at: <time>{{$post['created_at']}}</time></p> 
+    <p>Last Update: <time>{{$post['updated_at']}}</time></p> 
+
+    <hr>
+    <p>{{$post['content']}}</p>
 </article>
-
-{{-- @if ($status != "404")    
-
-@else
-    <p> Posts not found :(</p>    
-@endif --}}
+@endif
