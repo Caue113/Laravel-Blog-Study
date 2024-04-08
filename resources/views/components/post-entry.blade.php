465211f1cd83ea38055f23a322c048c36dd21d39
Luis Felipe <luisftsat@gmail.com>
2024-04-08 11:01:57 -0300

    feat: Created a tag component for post entry
    This commit adds a component inside a component, showing the capability of a parent component passing data to children component. Data comes from Database, which is logically handled by the parent component

--- resources/views/components/post-entry.blade.php
+++ resources/views/components/post-entry.blade.php
@@ -1,5 +1,10 @@
 @props(['post'])
 
+@php
+    $tagNames = $post['tags'];
+    $tagNames = explode(",", $tagNames)
+@endphp
+
 <article {{$attributes->merge(['style' => "border: black 1px dashed;"])}}>
     <p>post_id: {{$post['id']}}</p>
     <p>post_title: {{$post['title']}}</p>
@@ -8,6 +13,19 @@
     <p>Posted at: <time>{{$post['created_at']}}</time></p> 
     <p>Last Update: <time>{{$post['updated_at']}}</time></p> 
 
+    <br>
+
+    {{-- Tag List --}}
+    <div style="display: flex; flex-flow: row wrap; gap: 0.25rem; margin: 0rem 0.25rem">
+    @foreach ($tagNames as $tagName)
+        <x-post-tag :tagName="$tagName" />
+    @endforeach
+    </div>
+   
+
+    {{-- <p><i>{{$post['tags']}}</i></p> --}}
+
     <hr>
     <p>{{$post['content']}}</p>
+    
 </article>
---
+++ resources/views/components/post-tag.blade.php
@@ -0,0 +1,4 @@
+@props(["tagName"])
+<div style="background-color: #0f0f0f; color: #ffffff; flex: 1 1 0px; text-align: center; padding: 0.5rem; width: min-content">
+    <p style="margin: 0px">{{$tagName}}</p>    
+</div>
