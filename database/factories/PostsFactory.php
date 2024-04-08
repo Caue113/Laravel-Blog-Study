a0a7fad38c60e33d12f9f378540300e8dcc96f67
Luis Felipe <luisftsat@gmail.com>
2024-04-08 20:20:40 -0300

    feat: DB Seed in posts now have tags variation
    Tags will be needed to filter posts when click on them. DB Seeding with different tags is useful to provide variety

--- database/factories/PostsFactory.php
+++ database/factories/PostsFactory.php
@@ -16,10 +16,24 @@ class PostsFactory extends Factory
      */
     public function definition(): array
     {
+        //Generate CSV Data Randomly
+        $TAGS = ["Novel", "Travel", "Mathematics", "Statistics", "Literature", "Programming", "Self Story", "Art", "Design", "Music", "Illustration", "Joke", "Web Design", "Web Development", "Photography", "News", "Guide", "Tutorial"];
+        $attempts = rand(1, 5); //1 to 5 tags can be used
+        $tags = [];
+
+        for ($i=0; $i < $attempts ; $i++) { 
+            $tag = $TAGS[rand(0, sizeof($TAGS) - 1)];
+            if(array_search($tag, $tags) === false){
+                array_push($tags, $tag);
+            }
+        }
+
+        $tagsCsv = implode(",", $tags);
+
         return [
             "title" => $this->faker->sentence(),
             "subtitle" => $this->faker->sentence(18),
-            "tags" => "Novel, Travel, Mathematics, Statistics, Literature, Programming",
+            "tags" => $tagsCsv,
             "content" => $this->faker->realText(150),
             "owner" => $this->faker->name(),
             "created_at" => date("d/m/Y H:i:s", time()),
