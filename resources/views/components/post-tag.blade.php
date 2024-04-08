b8a1f1ecb0bf017f14c464b9f898c9dd2dce102d
Luis Felipe <luisftsat@gmail.com>
2024-04-08 11:05:23 -0300

    hotfix: style of tags now properly wraps around content, not parent flex container

--- resources/views/components/post-tag.blade.php
+++ resources/views/components/post-tag.blade.php
@@ -1,4 +1,4 @@
 @props(["tagName"])
-<div style="background-color: #0f0f0f; color: #ffffff; flex: 1 1 0px; text-align: center; padding: 0.5rem; width: min-content">
+<div style="background-color: #0f0f0f; color: #ffffff; text-align: center; padding: 0.5rem 0.75rem; width: min-content; border-radius: 1rem">
     <p style="margin: 0px">{{$tagName}}</p>    
 </div>
